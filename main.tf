525fd195ed4e8f95389b6521d854dc405787a80a
ShayA <someonenewnew7@gmail.com>
2023-05-21 18:39:20 +0300

    terraform7

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "myec233" {
-  ami = ""
+  ami = "ami-0aa2b7722dc1b5612"
   instance_type = "t2.micro"
   tags = {
     Name = "terra-jenkins"
